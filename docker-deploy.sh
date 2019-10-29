ab10fd4278dc06fb1c31fdf15bdaa509be718c57
shindongsun0 <shindongsun0@naver.com>
2019-10-29 16:47:56 +0000

    creating docker image

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,5 +1,5 @@
 docker login -u $DOCKER_ID -p $DOCKER_PASSWORD
-docker build -t rstsns .
-docker run --name rstsns-container -p 9000:9000 -d rstsns
-docker tag rstsns sds111888/rstsns
-docker push sds111888/rstsns
+docker build -t docker-node-express-mongoapp .
+docker run --name docker-node-express-mongoapp -p 9000:9000 -d docker-node-express-mongoapp
+docker tag docker-node-express-mongoapp sds111888/docker-node-express-mongoapp
+docker push sds111888/docker-node-express-mongoapp
