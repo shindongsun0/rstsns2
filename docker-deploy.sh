d56bbfffd12b93c4f22254c6bfd5bffd189dcd51
shindongsun0 <shindongsun0@naver.com>
2019-10-29 17:14:48 +0000

    dongsun editing index.jade

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -3,3 +3,4 @@ docker build -t docker-node-express-mongoapp .
 docker run --name docker-node-express-mongoapp -p 9000:9000 -d docker-node-express-mongoapp
 docker tag docker-node-express-mongoapp sds111888/docker-node-express-mongoapp
 docker push sds111888/docker-node-express-mongoapp
+
