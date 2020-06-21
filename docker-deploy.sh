31fb3cc4e3a07c64f02483a42e5104a8fc9e67b6
Dongsun <52663248+shindongsun0@users.noreply.github.com>
2020-06-21 12:14:14 +0900

    Update docker-deploy.sh

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,6 +1,8 @@
 docker login -u $DOCKER_ID -p $DOCKER_PASSWORD
+docker rmi docker-node-express-mongoapp sds111888/docker-node-express-mongoapp
 docker build -t docker-node-express-mongoapp .
 docker run --name docker-node-express-mongoapp -p 9000:9000 -d docker-node-express-mongoapp
 docker tag docker-node-express-mongoapp sds111888/docker-node-express-mongoapp
 docker push sds111888/docker-node-express-mongoapp
+
 
